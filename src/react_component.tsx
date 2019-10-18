04728c1df24d9e93de6d4b30be673be308f62a3c
dyb881 <15118995517@163.com>
2019-10-18 10:44:50 +0800

    使用新的组件

--- src/react_component.tsx
+++ src/react_component.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 interface IProps {
   name: string; // 动画标识名
   time?: number; // 动画时长
   enterTime?: number; // 进入动画时长
-  leaveTime?: number; // 离开动画时长
-  [key: string]: any;
+  exitTime?: number; // 离开动画时长
 }
 
 /**
  * 组件进出动画
  */
-const Transition: React.SFC<IProps> = ({ name, time = 400, enterTime, leaveTime, ...props }) => (
-  <ReactCSSTransitionGroup
-    transitionName={name}
-    transitionEnterTimeout={enterTime || time}
-    transitionLeaveTimeout={leaveTime || time}
-    {...props}
-  />
+const Transition: React.SFC<IProps> = ({ name, time = 400, enterTime = time, exitTime = time, children }) => (
+  <TransitionGroup>
+    {React.Children.map(children, child => (
+      <CSSTransition key={(child as any).key} classNames={name} timeout={{ enter: enterTime, exit: exitTime }}>
+        {child}
+      </CSSTransition>
+    ))}
+  </TransitionGroup>
 );
 
 export default Transition;
